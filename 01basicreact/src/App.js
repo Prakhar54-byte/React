ed6a45f04fa148f5a1b0ff8f642abd8efec51273
Prakhar54-byte <prakharchauhan179@gmail.com>
2024-06-18 01:05:31 +0530

    React lec 1

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Chai from "./Chai"
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h3>
+      Hello
+    </h3>
+
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,13 @@
+
+
+function Chai(){
+    return(
+        <div>
+            <h1>
+                Chai is ready
+            </h1>
+        </div>
+    )
+}
+
+export default Chai
