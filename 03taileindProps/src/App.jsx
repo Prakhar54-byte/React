064a70f4d2bd1bc576bed9daa6065bcc72e624ee
Prakhar54-byte <prakharchauhan179@gmail.com>
2024-06-19 00:10:37 +0530

    Props and tailwind

---
+++ 03taileindProps/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/card";
+function App() {
+  let myObj = {
+    username: "Prakhar",
+    age: 19,
+  };
+  let myArr = [1, 2, 3, 1, 5, 6, 4];
+
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl"> TailWind Test</h1>
+      <Card username="chai aur code" someObj={myObj} btnText="click me " />
+      <Card username="Prkahar" newArr={myArr} btnText="visit me" />
+    </>
+  );
+}
+
+export default App;
