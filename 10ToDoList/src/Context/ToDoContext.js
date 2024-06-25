48ae0f5a0773b3108fd1a588db3125961f423299
Prakhar54-byte <prakharchauhan179@gmail.com>
2024-06-25 17:24:47 +0530

    todo list

---
+++ 10ToDoList/src/Context/ToDoContext.js
@@ -0,0 +1,46 @@
+// import { createContext, useContext } from "react";
+
+// export const TodoContext = createContext({
+//     todos: [
+//         {
+//             id: 1,
+//             todo: "Tod mesg",
+//             completed: false,
+//         }
+//     ],
+//     addTodo: (todo) => { },
+//     updateTod: (id, todo) => { },
+//     deleteTodo: (id) => { },
+//     toggleCompleted: (id) => { }
+// })
+
+
+// export const useTodo = () => {
+//     return useContext(TodoContext)
+// }
+
+
+// export const Todoprovider = TodoContext.Provider
+import {createContext, useContext} from "react"
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: " Todo msg",
+            completed: false,
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
+
