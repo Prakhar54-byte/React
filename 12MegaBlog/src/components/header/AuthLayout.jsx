494a09b40dca431a42df5731572dfb8ee37abddc
Prakhar54-byte <prakharc295@gmail.com>
2024-07-11 22:14:47 +0530

    make a post form

--- 12MegaBlog/src/components/header/AuthLayout.jsx
+++ 12MegaBlog/src/components/header/AuthLayout.jsx
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 export default function Protected({ children, authentication = true }) {
-
   const navigate = useNavigate();
   const [loader, setLoader] = useState(true);
   const authStatus = useSelector((state) => state.auth.status);
@@ -16,5 +15,5 @@ export default function Protected({ children, authentication = true }) {
     }
     setLoader(false);
   }, [authStatus, navigate, authentication]);
-  return loader? <h1>Loading...</h1> : <>{children}</>
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
 }
--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,9 +3,19 @@ import Footer from "./Footer/Footer";
 import Contanier from "./Container/Container"
 import Logo from "./Logo";
 import LogOutBtn from './header/LogOutBtn'
+import Select from './Select'
+import RTE from "./RTE";
+import SignUp from './SignUp'
+import Button from './Button'
+import Input from './Input'
 
 export {
+    Input,
+    Button,
     Header,
+    Select,
+    RTE,
+    SignUp,
     Footer,
     Contanier,
     Logo,
