b3df811ad9fcc8f937f663733ee9aac69441a652
Prakhar54-byte <prakharc295@gmail.com>
2024-07-11 19:10:10 +0530

    make signUp and login page

--- 12MegaBlog/src/components/Input.jsx
+++ 12MegaBlog/src/components/Input.jsx
@@ -5,11 +5,11 @@ const Input = React.forwardRef(
     const id = useId();
     return (
       <div className="w-full">
-        {lable && (
+        {lable && 
           <lable className="inline-block mb-1 pl-1 " htmlFor={id}>
             {lable}
           </lable>
-        )}
+        }
         <input
           type={type}
           className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
